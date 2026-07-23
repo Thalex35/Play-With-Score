bd5b5f31eaae3deea5806563ee86f9414a0db812
TheedDev <louisjuste.theodore.jr@gmail.com>
2026-07-23 16:43:03 -0400

    reset button implementation

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const addBtn = document.querySelector(".addBtn");
 const subBtn = document.querySelector(".subBtn");
 const submitAdd = document.querySelector(".submitAdd");
 const submitSub = document.querySelector(".submitSub");
+const resetBtn = document.getElementById("resetBtn");
 
 
 // inputs container
@@ -33,7 +34,24 @@ function playWithScore(){
         scoreArea.textContent = score;
     }
 
-    return {addOnScore, subOnScore}
+    function resetscore(){
+        score = 0;
+        scoreArea.textContent = score;
+    }
+
+    function updateScore(){
+        scoreArea.textContent = score;
+        if (score !== 0){
+            resetBtn.classList.remove("hidden");
+        }
+        if(score === 0) {
+            resetBtn.classList.add("hidden")
+        }
+
+    }
+    
+
+    return {addOnScore, subOnScore, resetscore, updateScore}
 }
 
 const wholeScore = playWithScore();
@@ -41,22 +59,36 @@ const wholeScore = playWithScore();
 submitAdd.addEventListener("click", (event) => {
     event.preventDefault();    
 
-    const addingPointsInput = document.getElementById("addingPoints").value;
-    let value = Number(addingPointsInput);
+    const addingPointsInput = document.getElementById("addingPoints");
+    let value = Number(addingPointsInput.value);
 
     if ( !isNaN(value)){
         wholeScore.addOnScore(value);
     }    
+
+    wholeScore.updateScore();
+    addingPointsInput.value = "";
 })
 
 submitSub.addEventListener("click", (event) => {
     event.preventDefault();
 
-    const substractPointsInput = document.getElementById("substractPoints").value;
-    let value = Number(substractPointsInput);
+    const substractPointsInput = document.getElementById("substractPoints");
+    let value = Number(substractPointsInput.value);
 
     if (!isNaN(value)) {
         wholeScore.subOnScore(value);
     }
 
+    wholeScore.updateScore();
+    substractPointsInput.value = "";
+
 })
+
+resetBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    wholeScore.resetscore();
+    wholeScore.updateScore();
+})
+
